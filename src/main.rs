2f1427364c8b7e6ca70b0a12f6d7744cddf2dbd6
jarone <jarone@vip.qq.com>
2020-08-05 17:42:09 +0800

    initialization

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("猜数字游戏欢迎您!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("请输入您猜的数字（1到100之间，包含1和100）");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("您猜的数字是: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了!"),
+            Ordering::Greater => println!("太大了!"),
+            Ordering::Equal => {
+                println!("您猜中了!");
+                break;
+            }
+        }
+    }
+}
