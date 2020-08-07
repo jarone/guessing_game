78c0ee89674ed0b65b6513d1f59910d0adffcd8f
jarone <jarone@vip.qq.com>
2020-08-07 21:19:23 +0800

    Show the number of guesses in the correct result

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("猜数字游戏欢迎您!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut guess_times = 0;
 
     loop {
         println!("请输入您猜的数字（1到100之间，包含1和100）");
@@ -21,13 +22,14 @@ fn main() {
             Err(_) => continue,
         };
 
+        guess_times = guess_times + 1;
         println!("您猜的数字是: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("太小了!"),
             Ordering::Greater => println!("太大了!"),
             Ordering::Equal => {
-                println!("您猜中了!");
+                println!("您猜中了! 共猜了 {} 次", guess_times);
                 break;
             }
         }
